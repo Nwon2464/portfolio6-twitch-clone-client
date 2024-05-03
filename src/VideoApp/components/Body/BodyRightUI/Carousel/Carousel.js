65bfe825e96bd6543faf170a992643fa34d22386
Namkyu Won <64752079+Nwon2464@users.noreply.github.com>
2024-05-03 16:22:23 +0900

    Update Carousel.js

--- src/VideoApp/components/Body/BodyRightUI/Carousel/Carousel.js
+++ src/VideoApp/components/Body/BodyRightUI/Carousel/Carousel.js
@@ -16,7 +16,7 @@ const Carousel = (props) => {
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get(
-        "server-t.vercel.app/api/v1/twitch"
+        "https://server-t.vercel.app/api/v1/twitch"
       );
       setGetTwitchLiveStream(data);
     };
